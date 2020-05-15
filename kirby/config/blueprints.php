6504c4bfbdedb2404baa023561bfe28f940ac4a6
Fernando Gutierrez <fernando@beeb.mx>
2020-05-15 12:27:41 -0500

    Update oxxo styles

--- kirby/config/blueprints.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'files/default' => __DIR__ . '/blueprints/file.yml',
-    'pages/default' => __DIR__ . '/blueprints/page.yml',
-    'site'          => __DIR__ . '/blueprints/site.yml'
-];
